29146146340983cd7a4668ec57cd32cebfa36d2b
Gustavo <rustdelima23@gmail.com>
2024-10-24 10:55:33 -0300

    feat: Adicionando drawer e espaçamento entre os campos

--- lib/pages/perfil_page.dart
+++ lib/pages/perfil_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:project_world_skills_prova04/preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class PerfilPage extends StatefulWidget {
   const PerfilPage({Key? key}) : super(key: key);
@@ -8,101 +10,47 @@ class PerfilPage extends StatefulWidget {
 }
 
 class _PerfilPageState extends State<PerfilPage> {
+
   final TextEditingController nameController = TextEditingController();
   final TextEditingController cityController = TextEditingController();
   final TextEditingController courseController = TextEditingController();
+  final Preferences preferences = Preferences();
+
+  setFields() async {
+    final name = await preferences.getString("name");
+    final city = await preferences.getString("city");
+    final course = await preferences.getString("course");
+
+    print(name);
+
+    setState(() {
+      nameController.text = name!;
+      cityController.text = city!;
+      courseController.text = course!;
+    });
+  }
+
+  @override
+  void initState() {
+    setFields();
+    super.initState();
+  }
+
+
+
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(),
-      drawer: Drawer(
-        child: Column(
-          children: [
-            SizedBox(
-              width: 20,
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              child: Image.asset("./assets/Logo.png"),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Container(
-              alignment: Alignment.center,
-              width: double.infinity,
-              height: 100,
-              decoration: BoxDecoration(color: Colors.blue[100]),
-              child: Text(
-                "Meu perfil",
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.pushReplacementNamed(context, "/ranking");
-              },
-              child: Container(
-                alignment: Alignment.center,
-                width: double.infinity,
-                height: 100,
-                decoration: BoxDecoration(color: Colors.white),
-                child: Text(
-                  "Ranking",
-                  style: TextStyle(color: Colors.black),
-                ),
-              ),
-            ),
-            Container(
-              alignment: Alignment.center,
-              width: double.infinity,
-              height: 100,
-              decoration: BoxDecoration(color: Colors.white),
-              child: Text(
-                "Disciplinas",
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-            SizedBox(
-              height: 350,
-            ),
-            Container(
-              height: 40,
-              decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.all(Radius.circular(8))),
-              width: 120,
-              child: Row(
-                children: [
-                  SizedBox(
-                    width: 12,
-                  ),
-                  Icon(
-                    Icons.logout,
-                    color: Colors.white,
-                  ),
-                  SizedBox(
-                    width: 14,
-                  ),
-                  Text(
-                    "Log Out",
-                    style: TextStyle(color: Colors.white),
-                  )
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
       body: Padding(
         padding: const EdgeInsets.all(32.0),
         child: Column(
           children: [
             Container(
               alignment: Alignment.center,
-              child: Column(
+              child: const Column(
                 children: [
                   Icon(
                     Icons.account_circle_outlined,
@@ -115,7 +63,7 @@ class _PerfilPageState extends State<PerfilPage> {
                 ],
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 20,
             ),
             TextFormField(
@@ -131,7 +79,7 @@ class _PerfilPageState extends State<PerfilPage> {
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(12)))),
             ),
-            SizedBox(
+            const SizedBox(
               height: 32,
             ),
             TextFormField(
@@ -147,7 +95,7 @@ class _PerfilPageState extends State<PerfilPage> {
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(12)))),
             ),
-            SizedBox(
+            const SizedBox(
               height: 32,
             ),
             TextFormField(
@@ -161,26 +109,28 @@ class _PerfilPageState extends State<PerfilPage> {
               controller: courseController,
               decoration: InputDecoration(
                   filled: true,
-                  fillColor: Colors.grey[100],
-                  label: Text("Curso"),
-                  border: OutlineInputBorder(
+                  fillColor: Colors.grey[300],
+                  label: const Text("Curso"),
+                  border: const OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(12)))),
             ),
-            SizedBox(
+            const SizedBox(
               height: 32,
             ),
             ElevatedButton(
                 style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.black,
                     foregroundColor: Colors.white),
-                onPressed: () {},
+                onPressed: () async {
+                  await setFields();
+                },
                 child: Container(
                   width: 120,
                   height: 30,
                   alignment: Alignment.center,
                   child: const Text("Mudar senha"),
                 )),
-            SizedBox(
+            const SizedBox(
               height: 20,
             ),
             ElevatedButton(
@@ -195,6 +145,91 @@ class _PerfilPageState extends State<PerfilPage> {
                   child: const Text("Atualizar"),
                 )),
           ],
+        ),
+      ),
+      drawer: Drawer(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                width: 20,
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                child: Image.asset("./assets/Logo.png"),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Container(
+                alignment: Alignment.center,
+                width: double.infinity,
+                height: 100,
+                decoration: BoxDecoration(
+                    color: Colors.blue[100], border: Border.all(width: 1)),
+                child: const Text(
+                  "Meu perfil",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.pushReplacementNamed(context, "/ranking");
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  width: double.infinity,
+                  height: 100,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: const Text(
+                    "Ranking",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                width: double.infinity,
+                height: 100,
+                decoration: const BoxDecoration(color: Colors.white),
+                child: const Text(
+                  "Disciplinas",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              const SizedBox(
+                height: 350,
+              ),
+              Container(
+                height: 40,
+                decoration: const BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.all(Radius.circular(8))),
+                width: 120,
+                child: const Row(
+                  children: [
+                    SizedBox(
+                      width: 12,
+                    ),
+                    Icon(
+                      Icons.logout,
+                      color: Colors.white,
+                    ),
+                    SizedBox(
+                      width: 14,
+                    ),
+                    Text(
+                      "Log Out",
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
